9cc08aba444dd34319469285932cb904623f8f7d
felipemunevarn <afmunene@gmail.com>
2022-02-08 14:29:14 -0300

    creating board and board view objects, and main function to initialize them

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-// test
+(function (){
+    var self.Board = function (width, height){
+        this.width = width;
+        this.height = height;
+        this. playing = false;
+        this.game_over =false;
+    }
+
+    self.Board.prototype = {
+        get elements(){
+            var elements = this.bars;
+            elements.push(ball);
+            return elements;
+        }
+    }
+
+})();
+
+(function (){
+    self.BoardView = function(canvas, board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+})();
+
+window.addEventListener("load", main);
+
+function main(){
+    var board = new Board(800, 400);
+    var canvas = document.getElementById("canvas");
+    var board_view = new BoardView(canvas, board);
+}
