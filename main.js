73d5226b61573b3ffeaddc0f8272a2e1157661a6
felipemunevarn <afmunene@gmail.com>
2022-02-10 17:35:10 -0300

    make the ball

--- main.js
+++ main.js
@@ -12,9 +12,23 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            // elements.push(this.ball);
+            elements.push(this.ball);
             return elements;
         }
+    }
+})();
+
+(function(){
+    self.Ball = function(x, y, radius, board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
     }
 })();
 
@@ -54,23 +68,35 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0, 0, board.width, board.height);
+        },
         draw: function (){
             for (let i = this.board.elements.length - 1; i >= 0; i--) {
                 var el = this.board.elements[i];
 
                 draw(this.ctx, el);                
             }
+        },
+        play: function(){
+            board_view.clean();
+            board_view.draw();
         }
     }
 
     function draw(ctx, element){
         // if (element !== null && element.hasOwnProperty("kind")) {
-            switch(element.kind){
+        switch(element.kind){
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
                 break;
-            }
-        // }
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 3.15*2)
+                ctx.fill();
+                ctx.closePath();
+                break;
+        }
         
     }
 })();
@@ -80,6 +106,7 @@ var bar_2 = new Bar(5, 150, 20, 100, board);
 var bar = new Bar(775, 150, 20, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(400, 200, 10, board);
 
 window.requestAnimationFrame(controller);
 
@@ -94,13 +121,11 @@ document.addEventListener("keydown", function(ev) {
     } else if (ev.keyCode === 83) {
         bar_2.down();
     }
-    console.log("" + bar);
-    console.log("" + bar_2);
 });
 
 window.addEventListener("load", controller);
 
 function controller(){    
-    board_view.draw();
+    board_view.play();
     window.requestAnimationFrame(controller);
 }
